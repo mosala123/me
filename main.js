e0c8b5eeb225f1374204646876b32492eb88ccfb
mosala123 <149291901+mosala123@users.noreply.github.com>
2024-01-24 18:04:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var typed =document.getElementById("h2")
+var typed = new Typed("#auto-type", {
+    strings: ["Front-End dev", "Wed Dev"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
+  var typed = new Typed("#about-auto", {
+    strings: ["Ahmed Fathi", "front-end developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
+  // <!-- Google tag (gtag.js) -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "G-E82FZDBSE0");
